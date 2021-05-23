e311f63d2b8bb267849ce7ceb3d0d437b3bbb7e7
salah19984 <sebastianjaeger1984@hotmail.de>
2021-05-23 10:31:34 +0200

    fix in der while loop

--- Logik/Diashow.py
+++ Logik/Diashow.py
@@ -6,8 +6,8 @@ import glob
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 num = 0
-#path = "/home/salah/Downloads/"
-path = "/home/pi/pictures/"
+path = "/home/salah/Downloads/"
+#path = "/home/pi/pictures/"
 img = ""
 
 #list_of_files = glob.glob(path)
@@ -15,8 +15,6 @@ img = ""
 #latest_file = max(list_of_files, key= os.path.getctime)
 #print("latest file: " + str(latest_file))
 
-files = os.listdir(path)
-amount = len(files)
     
 # Configuration for the matrix
 options = RGBMatrixOptions()
@@ -29,29 +27,33 @@ options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafr
 matrix = RGBMatrix(options = options)
 
 # Make image fit our screen.
-print("Amount of Pictures: " + str(amount))
+#print("Amount of Pictures: " + str(amount))
 #loop to iterate through the picture folder over and over
-while num < amount and amount != 0:
-    img = files[num].title().lower()
-    if os.listdir(path) !=[]:
-        _img = path + img
-    else:
-        #_img = "/home/salah/Pictures/critical_failure.jpeg"    
-        _img = "/home/pi/Downloads/criticalfailure.jpeg"    
-    image = Image.open(_img)
-    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
-    matrix.SetImage(image.convert('RGB'))
-    num +=1
-    time.sleep(5)
-    if num == amount+1:
-        num = 0
-#only reached when there is a Problem
-matrix.SetImage(image.convert('RGB'))
-time.sleep(5)
-'''try:
-    print("Press CTRL-C to stop.")
-    while True:
-        time.sleep(100)
-except KeyboardInterrupt:
-    sys.exit(0)
-'''
+while True:
+    files = os.listdir(path)
+    amount = len(files)
+    num = 0
+    while num < amount and amount != 0:
+        img = files[num].title().lower()
+        if os.listdir(path) !=[]:
+            _img = path + img
+        else:
+            _img = "/home/salah/Pictures/critical_failure.jpeg"    
+            _img = "/home/pi/Downloads/criticalfailure.jpeg"    
+        image = Image.open(_img)
+        image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+        matrix.SetImage(image.convert('RGB'))
+        num +=1
+        time.sleep(5)
+        #if num == amount:
+            #num = 0
+    #only reached when there is a Problem
+    #matrix.SetImage(image.convert('RGB'))
+    #time.sleep(5)
+    '''try:
+        print("Press CTRL-C to stop.")
+        while True:
+            time.sleep(100)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    '''
