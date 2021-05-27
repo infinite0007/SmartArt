132b3e68c6a2f35144172d6e0afbc95900cb9f13
Julian Lingnau <50872737+infinite0007@users.noreply.github.com>
2021-05-27 18:49:45 +0200

    API: Styles GET funktioniert

--- WebApp/src/js/pages/Art.js
+++ WebApp/src/js/pages/Art.js
@@ -1,8 +1,12 @@
 require("../../stylesheets/_all.scss");
 import React from "react";
 import ParallaxComponent from "../components/Parallax/ParallaxComponent";
+import GetStyles from "../components/APICalls/GetStyles";
+// Store einbinden
+import mobxInteractionStore from "../stores/mobxInteractionStore"
 
 function Art() {
+  GetStyles(); // Hole Styles durch API-GET-Call und speichere sie in Mobx-Styles-Array
 
   const body = {
     background: '#0a2a43',
