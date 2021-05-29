185f3aad339942820458e203cbb5e50d503e2d2e
Julian Lingnau <50872737+infinite0007@users.noreply.github.com>
2021-05-29 18:43:49 +0200

    Buttons eingefügt für das Hochladen eines Bildes für Kunst bereich, welches dann gleich in Base64 Mithilfe einer dependency encodet wird. Bilder werden dann sofort auch visuell angezeigt

--- WebApp/src/js/pages/Art.js
+++ WebApp/src/js/pages/Art.js
@@ -3,15 +3,24 @@ import React, { useState } from "react";
 import {Container, Button, Image} from "react-bootstrap";
 import ParallaxComponent from "../components/Parallax/ParallaxComponent";
 import StyleImagesModal from "../components/Art/StyleImagesModal";
+import FileBase64 from "../utils/react-file-base64" // Dependency als Util da ich diese von Hand bearbeiten musste für meinen Fall, dient dazu um Files in meinem Fall Bilder als Base64 string dazustellen, also zu encoden.
 
 // Store einbinden
 import mobxInteractionStore from "../stores/mobxInteractionStore"
 
 function Art() {
 
-  const [modalShow, setModalShow] = React.useState(false);
+  const [uploadedFile, setUploadedFile] = useState({files: []});
+
+  const [modalShow, setModalShow] = useState(false);
 
   const [stylePicture, setStylePicture] = useState("") // Pfad zur Bilddatei die als Style ausgewählt wurde.
+
+  // Callback für Base64 Upload für Image
+  function getFiles(files) {
+    setUploadedFile({ files: files })
+    console.log(files);
+  }
 
   const body = {
     background: '#0a2a43',
@@ -22,10 +31,24 @@ function Art() {
     <div style={body}>
       <ParallaxComponent/>
 
-      <Container className="image-list" >
+      <Container>
+
+        <Button variant="warning">
+          Wähle ein Bild welches kombiniert wird<br/>
+          <FileBase64
+            multiple={true}
+            onDone={getFiles.bind(this)}
+          />
+        </Button>
+
+        <div className="text-center">
+          { uploadedFile.files.map((file, index) => {
+            return <Image src={file.base64} width="300" key={index} rounded />
+          }) }
+        </div>
 
         <Button variant="primary" onClick={() => setModalShow(true)}>
-        Wähle einen Style für die Kombination
+        Wähle einen Style für die Kombination aus
         </Button>
 
         <StyleImagesModal
@@ -34,7 +57,9 @@ function Art() {
           setstylepicture={setStylePicture}
         />
 
-        <Image src={stylePicture} width="300" rounded />
+        <div className="text-center">
+          <Image src={stylePicture} width="300" rounded />
+        </div>
       </Container>
 
     </div>
