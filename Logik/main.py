1d742667227706ba4b46d1c854f4b9c6f6c8f1c8
salah19984 <sebastianjaeger1984@hotmail.de>
2021-05-30 18:55:48 +0200

    Exception handling in main für Zeitparameter in Diashow hinzugefügt

--- Logik/main.py
+++ Logik/main.py
@@ -12,15 +12,23 @@ if __name__ == "__main__":
     print("      1 -   Diashow starten")
     print("      2 -   Neuestes Bild anzeigen")
     print("=========================================")
-
-    choice = input("Was willst du tun?\n")
     
-    if choice == "1":
-        slt = int(input("Wähle noch die Zeit zwischen den Bildern aus: \n"))
+    choice = matrixController.inputCheck(input("Was willst du tun?\n"))
+    if choice == 1:
+        slt = matrixController.inputCheck(input("Waehle noch die Zeit (in Sekunden) zwischen den Bildern aus: \n"))
+        #Fehlerabfrage bei viel zu langen Zeiten
+        while slt > (60*60*24):
+            print("Eingabe dauert laenger als 1 Tag!")
+            safety = input("Sicher dass es so lang sein soll? (y/n)\n").lower()
+            if safety == "n":
+                slt = matrixController.inputCheck(input("Waehle eine andere Zeit (in Sekunden): \n"))
+            elif safety =="y":
+                matrixController.startDiashow(matrix, path,slt)
+            else:
+                print("falsche Eingabe!")
+                slt = input("Bitte eine Zahl eingeben: \n")
+                
         matrixController.startDiashow(matrix, path,slt)
-    elif choice == "2":
-        #print(matrixController.newestPic(path))
+    elif choice == 2:
         matrixController.startSingleImageView(matrix, path)
-    
-    
     
