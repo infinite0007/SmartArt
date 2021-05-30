fb0cf0892905947827b96e308fe6146b7b492571
salah19984 <sebastianjaeger1984@hotmail.de>
2021-05-30 19:20:34 +0200

    Exceptionhandling verbessert

--- Logik/helper.py
+++ Logik/helper.py
@@ -2,7 +2,7 @@
 def intCheck(prompt):
     while True:
         try:
-            num = int(prompt)
+            num = int(input(prompt))
             return num
         except ValueError as e:
             print("Das war keine Zahl... Bitte nochmal versuchen")
@@ -10,7 +10,15 @@ def intCheck(prompt):
 def strCheck(prompt):
     while True:
         try:
-            _str = str(prompt)
+            _str = str(input(prompt))
             return _str
         except ValueError as e:
             print("Das war kein String... Bitte nochmal versuchen")
+
+def startUI():
+    print("=========================================")
+    print("Willkommen in der Bildergalerie Steuerung")
+    print("      Bitte einen Modus auswaehlen")
+    print("      1 -   Diashow starten")
+    print("      2 -   Neuestes Bild anzeigen")
+    print("=========================================")
--- Logik/main.py
+++ Logik/main.py
@@ -1,19 +1,15 @@
 import matrixController
 import helper
 if __name__ == "__main__":
-
+    #Festlegen des Pfades für die anzuzeigenden Bilder
     #path = "/home/salah/Downloads/"
     path = "/home/pi/SmartArt/WebApp/public/matrixPictures/"
+    #setzen der Optionen für die 64x64 Matrix 
     matrix = matrixController.setOptions()
 
-    print("=========================================")
-    print("Willkommen in der Bildergalerie Steuerung")
-    print("      Bitte einen Modus auswaehlen")
-    print("      1 -   Diashow starten")
-    print("      2 -   Neuestes Bild anzeigen")
-    print("=========================================")
+    helper.startUI()    
     
-    choice = helper.intCheck(input("Was willst du tun?\n"))
+    choice = helper.intCheck("Was willst du tun?\n")
     if choice == 1:
         slt = helper.intCheck(input("Waehle noch die Zeit (in Sekunden) zwischen den Bildern aus: \n"))
         #Fehlerabfrage bei viel zu langen Zeiten
@@ -21,14 +17,17 @@ if __name__ == "__main__":
             print("Eingabe dauert laenger als 1 Tag!")
             safety = input("Sicher dass es so lang sein soll? (y/n)\n").lower()
             if safety == "n":
-                slt = helper.intCheck(input("Waehle eine andere Zeit (in Sekunden): \n"))
+                slt = helper.intCheck("Waehle eine andere Zeit (in Sekunden): \n")
             elif safety =="y":
                 matrixController.startDiashow(matrix, path,slt)
             else:
                 print("falsche Eingabe!")
-                safety = helper.strCheck(input("Sicher dass es so lang sein soll? (y/n)\n").lower())
+                safety = helper.strCheck("Sicher dass es so lang sein soll? (y/n)\n").lower()
                 
         matrixController.startDiashow(matrix, path,slt)
     elif choice == 2:
         matrixController.startSingleImageView(matrix, path)
-    
+    elif choice != 1 or choice != 2:
+        print("Diese Auswahl existiert nicht (1 Versuch übrig)")
+        helper.startUI()
+        choice = helper.intCheck("Was willst du tun?\n")
