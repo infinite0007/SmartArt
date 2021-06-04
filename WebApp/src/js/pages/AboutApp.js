99f26851968c998bbe18957ef7ee2200ca678b73
Julian Lingnau <50872737+infinite0007@users.noreply.github.com>
2021-06-04 20:41:35 +0200

    instructions

--- WebApp/src/js/pages/AboutApp.js
+++ WebApp/src/js/pages/AboutApp.js
@@ -15,10 +15,12 @@ export default class AboutApp extends React.Component {
             </Col>
             <Col xs={6} md={4}>
               <p>
-                1. Laden Sie sich das aktuelle Projekt herunter über: <a href="https://github.com/" target="_blank">Github</a><br /><br />
-                2. Deployen und installieren Sie das Projekt auf ihrem Raspberry Pi<br /><br />
-                3. Geben Sie Ihren ...<br /><br />
-                4. Nun bekommen können sie mithilfe des Menüs die Modi wählen.
+                1. Clonen Sie sich das aktuelle Projekt über: <a href="https://github.com/" target="_blank">Github</a><br />
+                2. Wechseln sie in den Smartart Ordner<br />
+                3. Mit dem Befehl #npm install werden nun alle Dependencies heruntergeladen<br />
+                4. Im Ordner WebApp kann nun mit dem Befehl #npm start der Server zeitgleich mit der Website gestartet werden<br />
+                5. Die Website steht nun in ihrem Netz unter der in dem in der Konsole genannten Link zur verfügung<br />
+                6. Nun kann das Menü im Logik Ordner der Matrix mithilfe des Befehls #sudo python3 main.py gestartet werden
               </p>
             </Col>
           </Row>
