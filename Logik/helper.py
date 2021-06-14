02080b5806c6c2ca20e3f86620be98fe78de47c4
salah19984 <sebastianjaeger1984@hotmail.de>
2021-06-14 22:29:20 +0200

    umbau des main moduls umbau helper modul

--- Logik/helper.py
+++ Logik/helper.py
@@ -1,4 +1,6 @@
 import os
+import matrixController
+
 def startUI():
     print("=========================================")
     print("Willkommen in der Bildergalerie Steuerung")
@@ -50,4 +52,33 @@ def cleanup(path):
             except FileNotFoundError:
                 print(fileName)
                 return main()
+def main():
+  #Festlegen des Pfades fuer die anzuzeigenden Bilder
+    #path = "/home/salah/SmartArt/WebApp/public/matrixPictures/"
+    path = "/home/pi/SmartArt/WebApp/public/matrixPictures/"
+    #setzen der Optionen fuer die 64x64 Matrix 
+    matrix = matrixController.setOptions()
+
+    startUI()    
+    
+    choice = startCheck("Was willst du tun?\n")
+    if choice == 1:
+        slt = intCheck("Waehle noch die Zeit (in Sekunden) zwischen den Bildern aus: \n")
+        #Fehlerabfrage bei viel zu langen Zeiten
+        while slt > (60*60*24):
+            print("Eingabe dauert laenger als 1 Tag!")
+            safety = strCheck("Sicher dass es so lang sein soll? (Y,y/N,n)\n")
+            if safety == "n":
+                slt = intCheck("Waehle eine andere Zeit (in Sekunden): \n")
+            elif safety =="y":
+                matrixController.startDiashow(matrix, path,slt)
+            else:
+                safety = strCheck("Sicher dass es so lang sein soll? (Y,y/N,n)\n")
+                
+        matrixController.startDiashow(matrix, path,slt)
+    elif choice == 2:
+        matrixController.startSingleImageView(matrix, path)
+    elif choice == 3:
+        cleanup(path)
+
             
--- Logik/main.py
+++ Logik/main.py
@@ -1,37 +1,4 @@
-import matrixController
 import helper
-def main():
-  #Festlegen des Pfades fuer die anzuzeigenden Bilder
-    #path = "/home/salah/SmartArt/WebApp/public/matrixPictures/"
-    path = "/home/pi/SmartArt/WebApp/public/matrixPictures/"
-    #setzen der Optionen fuer die 64x64 Matrix 
-    matrix = matrixController.setOptions()
-
-    helper.startUI()    
-    
-    choice = helper.startCheck("Was willst du tun?\n")
-    if choice == 1:
-        slt = helper.intCheck("Waehle noch die Zeit (in Sekunden) zwischen den Bildern aus: \n")
-        #Fehlerabfrage bei viel zu langen Zeiten
-        while slt > (60*60*24):
-            print("Eingabe dauert laenger als 1 Tag!")
-            safety = helper.strCheck("Sicher dass es so lang sein soll? (Y,y/N,n)\n")
-            if safety == "n":
-                slt = helper.intCheck("Waehle eine andere Zeit (in Sekunden): \n")
-            elif safety =="y":
-                matrixController.startDiashow(matrix, path,slt)
-            else:
-                safety = helper.strCheck("Sicher dass es so lang sein soll? (Y,y/N,n)\n")
-                
-        matrixController.startDiashow(matrix, path,slt)
-    elif choice == 2:
-        try:
-            matrixController.startSingleImageView(matrix, path)
-        except KeyboardInterrupt:
-            return main()
-    elif choice == 3:
-        helper.cleanup(path)
-        return main()
     
 if __name__ == "__main__":
-    main()
+    helper.main()
